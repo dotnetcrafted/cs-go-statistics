a4884c29febf2120bd905a9d0fd49f0a83f13557
mrRadik <noname_rf@mail.ru>
2019-08-29 09:58:12 +0500

    all guns in gunsenum

--- CsStat.Domain/Definitions/GunsEnum.cs
+++ CsStat.Domain/Definitions/GunsEnum.cs
@@ -11,117 +11,173 @@ namespace CsStat.Domain.Definitions
 
         Bomb,
 
-        [LogValue("hkp2000")]
-        Hkp,
-
-        [LogValue("m4a1_silencer")]
-        [Description("Эмка с глушитилем")]
-        M4Silencer,
-
-        [LogValue("usp_silencer")]
-        [Description("USP с глушителем")]
-        UspSilencer,
-
-        [LogValue("m4a1")]
-        [Description("Эмка")]
-        M4,
-
-        [LogValue("ak47")]
-        [Description("Калаш")]
-        Ak,
-
-        [LogValue("aug")]
-        Aug,
-
-        [LogValue("hegrenade")]
-        [Description("Грена")]
-        He,
-
+        [Description("Glock-18")]
         [LogValue("glock")]
-        [Description("Глок")]
         Glock,
 
-        [LogValue("sg556")]
-        Sg,
-
-        [LogValue("mag7")]
-        Mag,
-
-        [LogValue("galilar")]
-        Galilar,
-
-        [LogValue("cz75a")]
-        Cz,
-
-        [LogValue("ump45")]
-        Ump,
-
-        [LogValue("mp5sd")]
-        [Description("Бомжа")]
-        Mp5,
-
-        [LogValue("famas")]
-        [Description("Фамас")]
-        Famas,
-
-        [LogValue("fiveseven")]
-        Fiveseven,
-
-        [LogValue("mp7")]
-        Mp7,
-
-        [LogValue("bizon")]
-        [Description("Бизон")]
-        Bizon,
-
-        [LogValue("mp9")]
-        Mp9,
-
-        [LogValue("p90")]
-        P90,
-
-        [LogValue("awp")]
-        [Description("Слон")]
-        Awp, 
-
-        [LogValue("ssg08")]
-        Ssg,
-
-        [LogValue("deagle")]
-        Deagle,
-
-        [LogValue("tec9")]
-        Tec,
-
-        [LogValue("revolver")]
-        Revolver,
-
-        [LogValue("nova")]
-        Nova,
-
-        [LogValue("g3sg1")]
-        G3sg1,
-
-        [LogValue("xm1014")]
-        xm1014,
-
-        [LogValue("p250")]
-        P250,
-
-        [LogValue("knife_t")]
-        Knife,
-
-        [LogValue("inferno")]
-        Inferno,
-
+        [Description("Dual Berettas")]
         [LogValue("elite")]
         Elite,
 
+        [Description("P250")]
+        [LogValue("p250")]
+        P250,
+
+        [Description("Tec-9")]
+        [LogValue("tec9")]
+        Tec9,
+
+        [Description("CZ75-Auto")]
+        [LogValue("cz75a")]
+        CZ75,
+
+        [Description("Desert Eagle")]
+        [LogValue("deagle")]
+        Deagle,
+
+        [Description("R8 Revolver")]
+        [LogValue("revolver")]
+        Revolver,
+
+        [Description("USP-S")]
+        [LogValue("usp_silencer")]
+        Usps,
+
+        [Description("P2000")]
+        [LogValue("hkp2000")]
+        P2000,
+
+
+        [Description("Five-Seven")]
+        [LogValue("fiveseven")]
+        FiveSeven,
+
+        [Description("Nova")]
+        [LogValue("nova")]
+        Nova,
+
+        [Description("XM1014")]
+        [LogValue("xm1014")]
+        Xml,
+
+        [Description("Sawed-Off")]
+        [LogValue("sawedoff")]
+        Sawedoff,
+
+        [Description("M249")]
+        [LogValue("m249")]
+        M249,
+
+        [Description("Negev")]
         [LogValue("negev")]
         Negev,
 
-        [LogValue("sawedoff")]
-        Sawedoff,
+        [Description("MAG-7")]
+        [LogValue("mag7")]
+        Mag,
+
+        [Description("MAC-10")]
+        [LogValue("mac10")]
+        Mac,
+
+        [Description("MP7")]
+        [LogValue("mp7")]
+        Mp7,
+
+        [Description("UMP-45")]
+        [LogValue("ump45")]
+        Ump,
+
+        [Description("P90")]
+        [LogValue("p90")]
+        P90,
+
+        [Description("PP-Bizon")]
+        [LogValue("bizon")]
+        Bizon,
+
+        [Description("MP9")]
+        [LogValue("mp9")]
+        Mp9,
+
+        [Description("Galil AR")]
+        [LogValue("galilar")]
+        Galil,
+
+        [Description("AK-47")]
+        [LogValue("ak47")]
+        Ak,
+
+        [Description("SSG 08")]
+        [LogValue("ssg08")]
+        Ssg,
+
+        [Description("SG 553")]
+        [LogValue("sg556")]
+        Sg,
+
+        [Description("AWP")]
+        [LogValue("awp")]
+        Awp,
+
+        [Description("G3SG1")]
+        [LogValue("g3sg1")]
+        G3sgl,
+
+        [Description("FAMAS")]
+        [LogValue("famas")]
+        Famas,
+
+        [Description("M4A4")]
+        [LogValue("m4a4")]
+        M4,
+
+        [Description("M4A1-S")]
+        [LogValue("m4a1_silencer")]
+        M4s,
+
+        [Description("AUG")]
+        [LogValue("aug")]
+        Aug,
+
+        [Description("Zeus x27")]
+        [LogValue("taser")]
+        Taser,
+
+        [Description("Molotov")]
+        [LogValue("molotov")]
+        Molotov,
+
+        [Description("Decoy Grenade")]
+        [LogValue("decoy")]
+        Decoy,
+
+        [Description("Flashbang")]
+        [LogValue("flashbang")]
+        Flash,
+
+        [Description("High Explosive Grenade")]
+        [LogValue("hegrenade")]
+        He,
+
+        [Description("Smoke Grenade")]
+        [LogValue("smokegrenade")]
+        Smoke,
+
+        [Description("Incendiary Grenade")]
+        [LogValue("incgrenade")]
+        Inc,
+
+        [Description("Knife")]
+        [LogValue("knife_t")]
+        KnifeT,
+
+        [Description("Knife")]
+        [LogValue("knife_ct")]
+        KnifeCT
+
+
+
     }
 }
 
