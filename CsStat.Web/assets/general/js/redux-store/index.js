7faf9d5fc6e24eef5528f2fca12decdf8b058389
hostile-d <host.chel@gmail.com>
2019-09-04 11:40:08 +0500

    npm package to use redux chrome extention

--- CsStat.Web/assets/general/js/redux-store/index.js
+++ CsStat.Web/assets/general/js/redux-store/index.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { createLogger } from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction';
 import rootReducer from '../redux-reducers';
 
 const initialState = {};
@@ -13,9 +14,8 @@ if (process.env.NODE_ENV !== 'production') {
 const store = createStore(
     rootReducer,
     initialState,
-    compose(
-        applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    composeWithDevTools(
+        applyMiddleware(...middleware)
     )
 );
 
