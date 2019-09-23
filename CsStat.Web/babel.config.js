aba622761912cdc1e387963cc6f7cd497fd583f5
hostile-d <host.chel@gmail.com>
2019-09-23 10:05:37 +0500

    add @babel/preset-typescript

--- CsStat.Web/babel.config.js
+++ CsStat.Web/babel.config.js
@@ -9,7 +9,8 @@ module.exports = {
         '@babel/plugin-transform-object-assign',
         '@babel/plugin-proposal-class-properties',
         '@babel/plugin-syntax-dynamic-import',
-        '@babel/plugin-proposal-object-rest-spread'
+        '@babel/plugin-proposal-object-rest-spread',
+        '@babel/preset-typescript'
     ],
     env: {
         production: {
