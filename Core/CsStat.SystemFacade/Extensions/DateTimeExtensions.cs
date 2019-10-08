1578db8bf0e2661c4a5013e0a396e5e0b6dc99c9
medprj <medprj@gmail.com>
2019-10-08 21:03:20 +0500

    refactoring

---
+++ Core/CsStat.SystemFacade/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CsStat.SystemFacade.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static string ToShortFormat(this DateTime dateTime)
+        {
+            return dateTime.ToString("MM/dd/yyyy");
+        }
+    }
+}
---
+++ CsStat.Web/Controllers/BaseController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+using CsStat.Web.Models;
+
+namespace CsStat.Web.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected override JsonResult Json(
+            object data,
+            string contentType,
+            System.Text.Encoding contentEncoding,
+            JsonRequestBehavior behavior)
+        {
+            return new JsonNetResult()
+            {
+                Data = data,
+                ContentType = contentType,
+                ContentEncoding = contentEncoding,
+                JsonRequestBehavior = behavior,
+            };
+        }
+    }
+}
