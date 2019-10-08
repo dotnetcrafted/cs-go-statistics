115b872553144cb156d0d9f56d4354f25b786d18
medprj <medprj@gmail.com>
2019-10-08 21:40:31 +0500

    refactoring // rename

--- CsStat.Web/Controllers/HomeController.cs
+++ CsStat.Web/Controllers/HomeController.cs
@@ -55,18 +55,18 @@ namespace CsStat.Web.Controllers
             };
         }
 
-        private static IEnumerable<PlayerStatsViewModel> GetPlayersStat(string dateFrom = "", string dateTo = "")
+        private static IEnumerable<PlayerStatsViewModel> GetPlayersStat(string dateFrom, string dateTo)
         {
-            var players = _playerRepository.GetStatsForAllPlayers(dateFrom, dateTo).ToList();
-            var steamIds = string.Join(",", players.Select(x => x.Player.SteamId).ToList());
+            var playersStat = _playerRepository.GetStatsForAllPlayers(dateFrom, dateTo).ToList();
+            var steamIds = string.Join(",", playersStat.Select(x => x.Player.SteamId).ToList());
             var avatars = _steamApi.GetAvatarUrlBySteamId(steamIds);
 
-            foreach (var player in players)
+            foreach (var stat in playersStat)
             {
-                player.Player.ImagePath = avatars.FirstOrDefault(x => x.Key == player.Player.SteamId).Value;
+                stat.Player.ImagePath = avatars.FirstOrDefault(x => x.Key == stat.Player.SteamId).Value;
             }
 
-            return Mapper.Map<List<PlayerStatsViewModel>>(players);
+            return Mapper.Map<List<PlayerStatsViewModel>>(playersStat);
         }
     }
 }
