dd3f16369d0d29fc0d3816127c7c6d4332b357ed
hostile-d <host.chel@gmail.com>
2019-10-16 19:06:58 +0500

    add date shortcuts

--- CsStat.Web/assets/components/players-data/ts/filter-form.tsx
+++ CsStat.Web/assets/components/players-data/ts/filter-form.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Form, DatePicker, Button } from 'antd';
+import { Form, DatePicker, Button, Row, Col } from 'antd';
 import { FormComponentProps } from 'antd/es/form';
 import moment, { Moment } from 'moment';
+import en_GB from 'antd/es/date-picker/locale/en_GB';
 
 const SERVER_DATE_FORMAT = 'MM/DD/YYYY';
 const USER_DATE_FORMAT = 'll';
@@ -13,54 +14,65 @@ class FilterForm extends React.Component<IFilterFormProps, FilterFormState> {
         dateToIsOpen: false
     };
 
-    private handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        const dateFrom = this.props.form.getFieldValue('date-picker-from');
-        const dateTo = this.props.form.getFieldValue('date-picker-to');
-        if (dateFrom && dateTo) {
-            const values: DateValues = {
-                dateFrom: dateFrom.format(SERVER_DATE_FORMAT),
-                dateTo: dateTo.format(SERVER_DATE_FORMAT)
-            };
-
-            this.props.onFormSubmit(values);
-        }
-    };
-
     render() {
         const { dateFrom, dateTo } = this.props;
         const { getFieldDecorator } = this.props.form;
 
         return (
-            <Form layout="inline" onSubmit={this.handleSubmit}>
-                <Form.Item>
-                    {getFieldDecorator('date-picker-from', this.getConfig(dateFrom))(
-                        <DatePicker
-                            format={USER_DATE_FORMAT}
-                            disabledDate={this.disabledDateFrom}
-                            onChange={this.onFromChange}
-                            onOpenChange={this.handleFromOpenChange}
-                            placeholder="From"
-                        />
-                    )}
-                </Form.Item>
-                <Form.Item>
-                    {getFieldDecorator('date-picker-to', this.getConfig(dateTo))(
-                        <DatePicker
-                            format={USER_DATE_FORMAT}
-                            disabledDate={this.disabledDateTo}
-                            onChange={this.onToChange}
-                            open={this.state.dateToIsOpen}
-                            onOpenChange={this.handleToOpenChange}
-                            placeholder="To"
-                        />
-                    )}
-                </Form.Item>
-                <Form.Item>
-                    <Button type="primary" htmlType="submit" disabled={this.props.isLoading}>
-                        Filter
-                    </Button>
-                </Form.Item>
+            <Form layout="inline" onSubmit={this.handleSubmit} className="filter-form">
+                <Row>
+                    <Col span={24}>
+                        <Form.Item>
+                            {getFieldDecorator('date-picker-from', this.getConfig(dateFrom))(
+                                <DatePicker
+                                    format={USER_DATE_FORMAT}
+                                    disabledDate={this.disabledDateFrom}
+                                    onChange={this.onFromChange}
+                                    onOpenChange={this.handleFromOpenChange}
+                                    placeholder="From"
+                                    locale={en_GB}
+                                />
+                            )}
+                        </Form.Item>
+                        <Form.Item>
+                            {getFieldDecorator('date-picker-to', this.getConfig(dateTo))(
+                                <DatePicker
+                                    format={USER_DATE_FORMAT}
+                                    disabledDate={this.disabledDateTo}
+                                    onChange={this.onToChange}
+                                    open={this.state.dateToIsOpen}
+                                    onOpenChange={this.handleToOpenChange}
+                                    placeholder="To"
+                                    locale={en_GB}
+                                />
+                            )}
+                        </Form.Item>
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit" disabled={this.props.isLoading}>
+                                Filter
+                            </Button>
+                        </Form.Item>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col span={24}>
+                        <Form.Item>
+                            <Button onClick={this.onDayButtonClick} disabled={this.props.isLoading}>
+                                Today
+                            </Button>
+                        </Form.Item>
+                        <Form.Item>
+                            <Button onClick={this.onWeekButtonClick} disabled={this.props.isLoading}>
+                                This Week
+                            </Button>
+                        </Form.Item>
+                        <Form.Item>
+                            <Button onClick={this.onMonthButtonClick} disabled={this.props.isLoading}>
+                                This Month
+                            </Button>
+                        </Form.Item>
+                    </Col>
+                </Row>
             </Form>
         );
     }
@@ -132,6 +144,51 @@ class FilterForm extends React.Component<IFilterFormProps, FilterFormState> {
     };
     private handleToOpenChange = (open: boolean): void => {
         this.setState({ dateToIsOpen: open });
+    };
+
+    private handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const dateFrom = this.props.form.getFieldValue('date-picker-from');
+        const dateTo = this.props.form.getFieldValue('date-picker-to');
+        if (dateFrom && dateTo) {
+            const values: DateValues = {
+                dateFrom: dateFrom.format(SERVER_DATE_FORMAT),
+                dateTo: dateTo.format(SERVER_DATE_FORMAT)
+            };
+
+            this.props.onFormSubmit(values);
+        }
+    };
+
+    private onDayButtonClick = () => {
+        const values: DateValues = {
+            dateFrom: moment().format(SERVER_DATE_FORMAT),
+            dateTo: moment().format(SERVER_DATE_FORMAT)
+        };
+
+        this.props.onFormSubmit(values);
+    };
+
+    private onWeekButtonClick = () => {
+        const values: DateValues = {
+            dateFrom: moment()
+                .startOf('isoWeek')
+                .format(SERVER_DATE_FORMAT),
+            dateTo: moment().format(SERVER_DATE_FORMAT)
+        };
+
+        this.props.onFormSubmit(values);
+    };
+
+    private onMonthButtonClick = () => {
+        const values: DateValues = {
+            dateFrom: moment()
+                .startOf('month')
+                .format(SERVER_DATE_FORMAT),
+            dateTo: moment().format(SERVER_DATE_FORMAT)
+        };
+
+        this.props.onFormSubmit(values);
     };
 }
 
