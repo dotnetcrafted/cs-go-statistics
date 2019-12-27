e3d996d2fb77f4a677ab51b5e0afd642a66a0c06
RadikF <radik.faiskhanov@deleteagency.com>
2019-12-27 17:42:36 +0500

    add build.bat

--- CsStat.Web/build-dev.bat
+++ CsStat.Web/build-dev.bat
@@ -1,4 +1,14 @@
 cls
-
+Echo "Building assets"
 call yarn
 call yarn run dev:build
+
+cls
+
+Echo "Building solution/project file using batch file"
+SET PATH=c:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin
+SET SolutionPath=d:\projects\counterstrikestat\Latest\CounterStrikeStat.sln
+Echo Start Time - %Time%
+MSbuild %SolutionPath% /p:Configuration=Debug /p:Platform="Any CPU"
+Echo End Time - %Time%
+Set /p Wait=Build Process Completed...
