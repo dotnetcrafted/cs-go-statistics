cf0a2be42b14d0bbfbc552bcc0262649a33f521b
hostile-d <host.chel@gmail.com>
2020-01-03 21:35:47 +0500

    update header styles

--- CsStat.Web/assets/components/base-layout/ts/base-layout.tsx
+++ CsStat.Web/assets/components/base-layout/ts/base-layout.tsx
@@ -16,15 +16,15 @@ const BaseLayout: SFC<BaseLayoutProps> = (props) => (
     <Layout className="base-layout__layout">
         <Header className="base-layout__header">
             <Row type="flex" justify="start" align="middle">
-                <Col xs={6} lg={1} className="base-layout__logo">
+                <Col xs={1} lg={1} className="base-layout__logo">
                     <span style={{ fontSize: '3rem', }}>🎄</span>
                 </Col>
-                <Col xs={6} lg={6}>
+                <Col xs={4} lg={4}>
                     <Title level={1} className="base-layout__title">
                         Fuse8 CS:GO Statistics
                     </Title>
                 </Col>
-                <Col xs={12} lg={6}>
+                <Col xs={10} lg={10}>
                     <Navigation/>
                 </Col>
             </Row>
