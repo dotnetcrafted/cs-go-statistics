eb594f42e0c29054c5a16d762e2375fc6ca1ad95
hostile-d <host.chel@gmail.com>
2020-01-03 21:52:30 +0500

    add placeholder on wiki page

--- CsStat.Web/assets/components/pages/ts/wiki-page.tsx
+++ CsStat.Web/assets/components/pages/ts/wiki-page.tsx
@@ -1,11 +1,9 @@
 import React, { SFC } from 'react';
+import { Empty } from 'antd';
 import '../scss/index.scss';
 
 const WikiPage: SFC<WikiPageProps> = (props) => (
-    <div className="wiki-page">
-        <h1>Wiki Page</h1>
-        <p>API URL: <a href={props.wikiUrl}>{props.wikiUrl}</a></p>
-    </div>
+    <Empty />
 );
 
 type WikiPageProps = {
