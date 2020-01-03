c35c1003a7458c43a9baf82ac40f4e8841ceb0e8
hostile-d <host.chel@gmail.com>
2020-01-03 15:52:41 +0500

    remove unused components

--- CsStat.Web/assets/components/players-data/index.ts
+++
@@ -1,6 +0,0 @@
-import './scss/index.scss';
-
-import { dcFactory } from '@deleteagency/dc';
-import PlayersDataComponent from './ts/players-data.component';
-
-dcFactory.register(PlayersDataComponent);
