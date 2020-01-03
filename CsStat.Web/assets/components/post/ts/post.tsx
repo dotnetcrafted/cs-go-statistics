ade56ea737e088d86070440a422ecd673ced9efc
hostile-d <host.chel@gmail.com>
2020-01-04 03:40:02 +0500

    add tags

--- CsStat.Web/assets/components/post/ts/post.tsx
+++ CsStat.Web/assets/components/post/ts/post.tsx
@@ -5,13 +5,31 @@ import moment from 'moment';
 import { Post as PostType, Tag as TagType } from '../../../general/ts/redux/types';
 
 const USER_DATE_FORMAT = 'll';
-
+const TAG_COLORS = [
+    'magenta',
+    'red',
+    'volcano',
+    'orange',
+    'gold',
+    'lime',
+    'green',
+    'cyan',
+    'blue',
+    'geekblue',
+    'purple'
+]
 const Post: SFC<PostProps> = (props) => {
+    const getRandomColor = (colors: string[]): string => {
+        return colors[Math.floor(Math.random() * colors.length)];
+    }
+
     const getTags = (tags: TagType[]): ReactNode => {
-        if (tags.length > 0) {
+        if (tags.length === 0) {
             return <span></span>;
         }
-        return tags.map((tag: TagType, index: number) => <Tag key={index} color="magenta">{tag.Caption}</Tag>);
+        return tags.map((tag: TagType, index: number) => {
+            return <Tag key={index} color={getRandomColor(TAG_COLORS)}>{tag.Caption}</Tag>;
+        });
     };
 
     const {
