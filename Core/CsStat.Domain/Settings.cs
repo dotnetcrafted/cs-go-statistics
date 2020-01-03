2671580aa79c81bcc16daa9530b15e1a9e317ef4
hostile-d <host.chel@gmail.com>
2020-01-03 21:10:33 +0500

    add MVC route

--- Core/CsStat.Domain/Settings.cs
+++ Core/CsStat.Domain/Settings.cs
@@ -13,6 +13,7 @@ namespace CsStat.Domain
         public static int TimerInterval => GetSetting(nameof(TimerInterval), Defaults.TimerInterval);
         public static int TakeLines => GetSetting(nameof(TakeLines), Defaults.TakeLines);
         public static string PlayersDataApiPath => GetSetting(nameof(PlayersDataApiPath), Defaults.PlayersDataApiPath);
+        public static string WikiPagePath => GetSetting(nameof(WikiPagePath), Defaults.WikiPagePath);
         public static string PlayersDataSteamApiPath => GetSetting(nameof(PlayersDataSteamApiPath), Defaults.PlayersDataSteamApiPath);
         public static long FirstSteamId => GetSetting(nameof(FirstSteamId), Defaults.FirstSteamId);
         public static string ApiKey => GetSetting(nameof(ApiKey), Defaults.ApiKey);
@@ -54,6 +55,7 @@ namespace CsStat.Domain
             public const int TimerInterval = 10000;
             public const int TakeLines = 50;
             public const string PlayersDataApiPath = "api/playersdata";
+            public const string WikiPagePath = "wiki";
             public const string PlayersDataSteamApiPath = @"http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/";
             public const long FirstSteamId = 76561197960265728;
             public const string ApiKey = "C03F2D79CF2FE20B64A85531031F3983";
