49802be4f75d38b333272f14cfeaa02e22bf970c
Sowa <Grimmino@gmail.com>
2020-01-04 11:53:07 +0500

    add github api contributors

--- CsStat.Web/assets/components/home-page/ts/authors-copyright.tsx
+++ CsStat.Web/assets/components/home-page/ts/authors-copyright.tsx
@@ -3,29 +3,82 @@ import { List, Avatar, Tag, Typography } from 'antd';
 
 const { Title } = Typography;
 
-const AuthorsCopyright = () => (
-    <List
-        size="small"
-        bordered={true}
-        header={<Title level={4}>Authors and Contributors:</Title>}
-        itemLayout="horizontal"
-        dataSource={data}
-        renderItem={person => (
-            <List.Item>
-                <List.Item.Meta
-                    avatar={<Avatar src={person.avatarSrc} />}
-                    title={
-                        <div>
-                            <a href={person.githubLink}>{person.name}</a> – <Tag>{person.role}</Tag>
-                        </div>
-                    }
-                />
-            </List.Item>
-        )}
-    />
-);
-export default AuthorsCopyright;
-const githubAccounts = {
+
+export default class AuthorsCopyright extends React.Component {
+
+    state ={
+        data: [],
+        role: []
+    }
+
+    gettingGithubAccounts = async () => {
+        const api_url = await 
+            fetch(`https://api.github.com/repos/dotnetcrafted/cs-go-statistics/contributors`)
+
+        const accounts = await api_url.json()
+
+        this.setState({
+            data: accounts.filter(elem => elem.type == "User")
+        })
+
+        this.state.data.forEach(item => {
+            this.gettingRole(item)
+        })
+    }
+
+    gettingRole = async (item) => {
+
+        const javaScriptCounter = 0
+        const cCounter = 0
+
+        const api_repos_url = await 
+            fetch(`${item.repos_url}`)
+
+        const repos = await api_repos_url.json()
+
+        repos.forEach(item => {
+            item.language == "JavaScript" ? javaScriptCounter = javaScriptCounter + 1 : null
+            item.language == "C#" ? cCounter = cCounter + 1 : null
+
+        });
+
+        console.log(item.login, "JavaScript =" + javaScriptCounter, "C# =" + cCounter)
+
+    }
+
+    componentWillMount = () => {
+        this.gettingGithubAccounts()
+    }
+
+    
+    render() {
+        const data = this.state.data != [] ? this.state.data : 'asdasd'
+        return (
+            <List
+                size="small"
+                bordered={true}
+                header={<Title level={4}>Authors and Contributors:</Title>}
+                itemLayout="horizontal"
+                dataSource={data}
+                renderItem={person => (
+                    <List.Item>
+                        <List.Item.Meta
+                            avatar={<Avatar src={person.avatar_url} />}
+                            title={
+                                <div>
+                                    <a href={person.url}>{person.login}</a> – <Tag>{person.role}</Tag>
+                                </div>
+                            }
+                        />
+                    </List.Item>
+                )}
+            />
+        )
+    }
+}
+
+
+/*const githubAccounts = {
     rdk174: 'Rdk174',
     medprj: 'Medprj',
     host: 'hostile-d'
@@ -50,4 +103,4 @@ const data = [
         avatarSrc: `https://github.com/${githubAccounts.host}.png`,
         role: 'frontend'
     }
-];
+];*/
