0c12741279f88775accfb5217a9174e60e70bd29
Sowa <Grimmino@gmail.com>
2020-01-06 14:59:36 +0500

    add eslint rules

--- CsStat.Web/assets/components/authors-copyright/ts/authors-copyright.tsx
+++ CsStat.Web/assets/components/authors-copyright/ts/authors-copyright.tsx
@@ -1,40 +1,60 @@
 import React from 'react';
-import { List, Avatar, Tag, Typography } from 'antd';
+import {
+    List, Avatar, Tag, Typography
+} from 'antd';
 
 const { Title } = Typography;
 
-
 export default class AuthorsCopyright extends React.Component {
-
     state ={
         data: [{
             avatar_url: '',
             url: '',
             login: ''
+        }],
+        roles: [{
+            login: '',
+            role: ''
         }]
     }
 
     private gettingGithubAccounts = async () => {
-        const accounts = await 
-            fetch(`https://api.github.com/repos/dotnetcrafted/cs-go-statistics/contributors`)
-                .then((res: Response) => res.json())
-                .then(acc => acc.filter((elem: { type: string; }) => elem.type == "User"))
-                .catch(err => {
-                        throw new Error(err)
-                })
+        const accounts = await fetch('https://api.github.com/repos/dotnetcrafted/cs-go-statistics/contributors')
+            .then((res: Response) => res.json())
+            .then((acc) => acc.filter((elem: { type: string; }) => elem.type === 'User'))
+            .catch((err) => {
+                throw new Error(err);
+            });
 
         this.setState({
             data: accounts
-        })
+        });
+
+        accounts.forEach((item: any) => {
+            this.gettingRole(item);
+        });
     }
 
-    componentWillMount = () => {
-        this.gettingGithubAccounts()
+    private gettingRole = async (item: any) => {
+        const languageArr: string[] = [];
+
+        const repos = await
+        fetch(`${item.repos_url}`)
+            .then((res: Response) => res.json());
+
+        repos.forEach((item: any) => {
+            item.language != null ? languageArr.push(item.language) : null;
+        });
+
+        console.log(languageArr);
     }
 
-    
+    UNSAFE_componentWillMount = () => {
+        this.gettingGithubAccounts();
+    }
+
     render() {
-        const data = this.state.data
+        const data = this.state.data;
         return (
             <List
                 size="small"
@@ -42,19 +62,19 @@ export default class AuthorsCopyright extends React.Component {
                 header={<Title level={4}>Authors and Contributors:</Title>}
                 itemLayout="horizontal"
                 dataSource={data}
-                renderItem={person => (
+                renderItem={(person) => (
                     <List.Item>
                         <List.Item.Meta
                             avatar={<Avatar src={person.avatar_url} />}
                             title={
                                 <div>
-                                    <a href={person.url}>{person.login}</a> – <Tag>Developer</Tag>
+                                    <a href={person.url}>{person.login}</a> – <Tag></Tag>
                                 </div>
                             }
                         />
                     </List.Item>
                 )}
             />
-        )
+        );
     }
 }
