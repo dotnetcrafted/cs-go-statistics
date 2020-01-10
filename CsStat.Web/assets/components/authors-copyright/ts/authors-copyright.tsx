c6bd8622092d5293d0ea9aaceb2a3d8242f3b067
Alex Mar <mayalexey@gmail.com>
2020-01-10 14:10:07 +0300

    update eslint rules, add types to authors, rework footer, authors

--- CsStat.Web/assets/components/authors-copyright/ts/authors-copyright.tsx
+++ CsStat.Web/assets/components/authors-copyright/ts/authors-copyright.tsx
@@ -1,53 +1,73 @@
-import React from 'react';
-import { List, Avatar, Tag, Typography } from 'antd';
+import React, { ReactNode } from 'react';
 
-const { Title } = Typography;
+const URL_REP = 'https://api.github.com/repos/dotnetcrafted/cs-go-statistics/contributors';
 
-const AuthorsCopyright = () => (
-    <List
-        size="small"
-        bordered={true}
-        header={<Title level={4}>Authors and Contributors:</Title>}
-        itemLayout="horizontal"
-        dataSource={data}
-        renderItem={person => (
-            <List.Item>
-                <List.Item.Meta
-                    avatar={<Avatar src={person.avatarSrc} />}
-                    title={
-                        <div>
-                            <a href={person.githubLink}>{person.name}</a> – <Tag>{person.role}</Tag>
-                        </div>
-                    }
-                />
-            </List.Item>
-        )}
-    />
-);
-export default AuthorsCopyright;
-const githubAccounts = {
-    rdk174: 'Rdk174',
-    medprj: 'Medprj',
-    host: 'hostile-d'
+export default class AuthorsCopyright extends React.Component<any, AuthorsCopyrightState> {
+    state = {
+        data: [
+            {
+                /* eslint-disable */
+                avatar_url: '',
+                html_url: '',
+                login: ''
+                /* eslint-enable */
+            }
+        ]
+    };
+
+    private getGithubAccounts = async () => {
+        const accounts: UserData[] = await fetch(URL_REP)
+            .then((res: Response) => res.json())
+            .then((acc) => acc.filter((elem: { type: string }) => elem.type === 'User'))
+            .catch((err) => {
+                throw new Error(err);
+            });
+
+        this.setState({
+            data: accounts
+        });
+    };
+
+    componentDidMount = () => {
+        this.getGithubAccounts();
+    };
+
+    render(): ReactNode {
+        const { data } = this.state;
+        return (
+            <div className="contributors">
+                <div className="contributors__title">
+                    <h4 className="contributors__title_txt">Authors and Contributors:</h4>
+                </div>
+
+                <div className="contributors__content">
+                    <ul className="contributors__list">
+                        {data.map((item, index) => (
+                            <li key={index} className="contributors__item">
+                                <a className="contributors__link" href={item.html_url}>
+                                    <div className="contributors__item_avatar">
+                                        <img src={item.avatar_url} alt=""/>
+                                    </div>
+
+                                    <div className="contributors__item_title">
+                                        {item.login}
+                                    </div>
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        );
+    }
+}
+
+type AuthorsCopyrightState = {
+    data: UserData[];
 };
 
-const data = [
-    {
-        name: 'Radik F',
-        githubLink: `https://github.com/${githubAccounts.rdk174}`,
-        avatarSrc: `https://github.com/${githubAccounts.rdk174}.png`,
-        role: 'backend'
-    },
-    {
-        name: 'Alexander M',
-        githubLink: `https://github.com/${githubAccounts.medprj}`,
-        avatarSrc: `https://github.com/${githubAccounts.medprj}.png`,
-        role: 'backend'
-    },
-    {
-        name: 'Danil S',
-        githubLink: `https://github.com/${githubAccounts.host}`,
-        avatarSrc: `https://github.com/${githubAccounts.host}.png`,
-        role: 'frontend'
-    }
-];
+type UserData = {
+    avatar_url: string;
+    html_url: string;
+    login: string;
+};
