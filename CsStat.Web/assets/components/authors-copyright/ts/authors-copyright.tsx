1bb80fa3107a47e7534acbbcb2dfcf5833a1f035
Sowa <Grimmino@gmail.com>
2020-01-10 16:01:54 +0500

    rework footer, authors

--- CsStat.Web/assets/components/authors-copyright/ts/authors-copyright.tsx
+++ CsStat.Web/assets/components/authors-copyright/ts/authors-copyright.tsx
@@ -1,7 +1,5 @@
 import React, { ReactNode } from 'react';
-import { List, Avatar, Typography } from 'antd';
 
-const { Title } = Typography;
 const URL_REP = 'https://api.github.com/repos/dotnetcrafted/cs-go-statistics/contributors';
 
 export default class AuthorsCopyright extends React.Component<any, AuthorsCopyrightState> {
@@ -37,35 +35,29 @@ export default class AuthorsCopyright extends React.Component<any, AuthorsCopyri
     render(): ReactNode {
         const { data } = this.state;
         return (
-            <List
-                size="small"
-                bordered={true}
-                header={
-                    <Title level={4}>Authors and Contributors:</Title>
-                }
-                itemLayout="horizontal"
-                grid={{
-                    gutter: 10,
-                    xl: 3,
-                    sm: 2,
-                    xs: 1
-                }}
-                dataSource={data}
-                renderItem={(person): ReactNode => (
-                    <List.Item style={{ marginTop: '10px' }}>
-                        <List.Item.Meta
-                            avatar={
-                                <Avatar icon="user" src={person.avatar_url} />
-                            }
-                            title={
-                                <div>
-                                    <a href={person.html_url}>{person.login}</a>
-                                </div>
-                            }
-                        />
-                    </List.Item>
-                )}
-            />
+            <div className="contributors">
+                <div className="contributors__title">
+                    <h4 className="contributors__title_txt">Authors and Contributors:</h4>
+                </div>
+
+                <div className="contributors__content">
+                    <ul className="contributors__list">
+                        {data.map((item, index) => (
+                            <li key={index} className="contributors__item">
+                                <a className="contributors__link" href={item.html_url}>
+                                    <div className="contributors__item_avatar">
+                                        <img src={item.avatar_url} alt=""/>
+                                    </div>
+
+                                    <div className="contributors__item_title">
+                                        {item.login}
+                                    </div>
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
         );
     }
 }
