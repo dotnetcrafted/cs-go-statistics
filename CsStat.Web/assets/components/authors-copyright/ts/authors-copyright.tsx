157ee421351061995b02fbc94ab55c1d90d9bec3
hostile-d <host.chel@gmail.com>
2020-01-10 10:55:51 +0500

    correct github user url

--- CsStat.Web/assets/components/authors-copyright/ts/authors-copyright.tsx
+++ CsStat.Web/assets/components/authors-copyright/ts/authors-copyright.tsx
@@ -1,39 +1,40 @@
 import React, { ReactNode } from 'react';
-import {
-    List, Avatar, Typography
-} from 'antd';
+import { List, Avatar, Typography } from 'antd';
 
 const { Title } = Typography;
 const URL_REP = 'https://api.github.com/repos/dotnetcrafted/cs-go-statistics/contributors';
 
 export default class AuthorsCopyright extends React.Component {
-    state ={
-        data: [{
-            avatar_url: '',
-            url: '',
-            login: ''
-        }]
-    }
+    state = {
+        data: [
+            {
+                avatar_url: '',
+                url: '',
+                login: ''
+            }
+        ]
+    };
 
     private getGithubAccounts = async () => {
         const accounts = await fetch(URL_REP)
             .then((res: Response) => res.json())
-            .then((acc) => acc.filter((elem: { type: string; }) => elem.type === 'User'))
-            .catch((err) => {
+            .then(acc => acc.filter((elem: { type: string }) => elem.type === 'User'))
+            .catch(err => {
                 throw new Error(err);
             });
 
         this.setState({
             data: accounts
         });
-    }
+    };
 
     componentDidMount = () => {
         this.getGithubAccounts();
-    }
+    };
 
     render() {
         const { data } = this.state;
+        console.log(data);
         return (
             <List
                 size="small"
@@ -47,7 +48,7 @@ export default class AuthorsCopyright extends React.Component {
                             avatar={<Avatar icon="user" src={person.avatar_url} />}
                             title={
                                 <div>
-                                    <a href={person.url}>{person.login}</a>
+                                    <a href={person.html_url}>{person.login}</a>
                                 </div>
                             }
                         />
