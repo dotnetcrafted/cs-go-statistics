7eef2e497d38362d0dac563fe1a75251c776d498
medprj <medprj@gmail.com>
2020-01-29 09:44:13 +0500

    added new project for collect cs statistic via http

---
+++ Utils/GameStateIntegration/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using CSGSI;
+using CSGSI.Nodes;
+
+namespace GameStateIntegration
+{
+    static class Program
+    {
+        static GameStateListener _gsl;
+        static void Main(string[] args)
+        {
+            _gsl = new GameStateListener(3000);
+            _gsl.NewGameState += OnNewGameState;
+            if (!_gsl.Start())
+            {
+                Console.WriteLine("here");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            
+            var timer = new Timer(Callback, null, 0, 10000);
+            
+            Console.WriteLine("Listening...");
+        }
+
+        private static void Callback(object state)
+        {
+            var a = _gsl.CurrentGameState;
+            var str = a != null ? a.JSON : "Failed";
+            Console.WriteLine(str);
+        }
+
+        static void OnNewGameState(GameState gs)
+        {
+            if (gs.Round.Phase == RoundPhase.Live &&
+                gs.Bomb.State == BombState.Planted &&
+                gs.Previously.Bomb.State == BombState.Planting)
+            {
+                Console.WriteLine("Bomb has been planted.");
+                Console.WriteLine(gs.JSON);
+            }
+        }
+    }
+}
