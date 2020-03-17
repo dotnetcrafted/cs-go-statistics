49ba8c25858321bea19c936980197b2f85f0e632
mrRadik <noname_rf@mail.ru>
2020-03-17 10:32:52 +0500

    achievements improvements

---
+++ Core/CsStat.Domain/Extensions/AchieveModelExtensions.cs
@@ -0,0 +1,13 @@
+﻿using CsStat.Domain.Models;
+
+namespace CsStat.Domain.Extensions
+{
+    public static class AchieveModelExtensions
+    {
+        public static AchieveModel ChangeDescription(this AchieveModel achieve, object value)
+        {
+           achieve.Description = string.Format(achieve.Description, value);
+           return achieve;
+        }
+    }
+}
