bf01f878b86ee636ab87ddf0ea44e8e841e8694e
medprj <medprj@gmail.com>
2020-03-24 14:39:39 +0500

    CT squad always placed on right

--- CsStat.Web/Controllers/MatchesController.cs
+++ CsStat.Web/Controllers/MatchesController.cs
@@ -167,7 +167,7 @@ namespace CsStat.Web.Controllers
                                 Adr = 0.0,
                                 Ud = 0.0
                             }).ToList()
-                        }).OrderByDescending(x => x.Title).ToList()
+                        }).OrderBy(x => x.Title).ToList()
                     }).ToList()
                 };
 
