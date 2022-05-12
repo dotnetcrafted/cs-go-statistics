5bb19c7335d401d12b6cc70b80527b6d91a82b19
Maksim Kadomtsev <skvigl@gmail.com>
2022-05-12 21:58:36 +0500

    Fix: match stats mobile view

--- CsStat.Web/assets/components/match/ts/enums.ts
+++ CsStat.Web/assets/components/match/ts/enums.ts
@@ -6,48 +6,55 @@ export const columns: ColumnProps<MatchPlayerModel>[] = [
         title: 'Player',
         dataIndex: 'name',
         key: 'name',
-        width: '35%'
+        width: '35%',
+        className: 'player-name'
     },
     {
         title: 'K/A/D',
         dataIndex: 'kad',
         key: 'kad',
         align: "center",
-        width: '15%'
+        width: '15%',
+        className: 'player-kad'
     },
     {
         title: 'KD Diff',
         dataIndex: 'kdDiff',
         key: 'kdDiff',
         align: "center",
-        width: '10%'
+        width: '10%',
+        className: 'player-stats'
     },
     {
         title: 'KD',
         dataIndex: 'kd',
         key: 'kd',
         align: "center",
-        width: '10%'
+        width: '10%',
+        className: 'player-stats'
     },
     {
         title: 'ADR',
         dataIndex: 'adr',
         key: 'adr',
         align: "center",
-        width: '10%'
+        width: '10%',
+        className: 'player-stats'
     },
     {
         title: 'UD',
         dataIndex: 'ud',
         key: 'ud',
         align: "center",
-        width: '10%'
+        width: '10%',
+        className: 'player-stats'
     },
     {
         title: 'Score',
         dataIndex: 'score',
         key: 'score',
         align: "center",
-        width: '10%'
+        width: '10%',
+        className: 'player-stats'
     }
 ];
